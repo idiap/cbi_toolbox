8a1149061fe69eed4bd820ff6b23fb054582a1fa
M. François <fran.marelli@gmail.com>
2019-04-21 20:12:46 +0200

    Matlab trikernel

---
+++ cbi_toolbox/splineradon/__init__.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+import cbi_toolbox.csplineradon as cradon
+
+
+def fft_trikernel(Nt, a, n1, n2, n3, h1, h2, h3, pad_fact):
+    T = a / (Nt - 1)
+    # TODO check this with Michael
+    dnu = 1 / (T * (pad_fact * Nt - 1))
+    nu = -1 / (2 * T) + np.arange(pad_fact * Nt) * dnu
+
+    trikernel_hat = np.power(np.sinc(np.outer(nu, h1)), (n1 + 1)) * np.power(
+        np.sinc(np.outer(nu, h2)), (n2 + 1)) * np.power(np.sinc(np.outer(nu, h3)), (n3 + 1))
+
+    kernel = np.abs(trikernel_hat)
+
+    return kernel[0:Nt, :] / (T * Nt * pad_fact)
+
+
+def get_kernel_table(Nt, n1, n2, h, s, angles, degree=True):
+    pad_fact = 4
+    angles = np.atleast_1d(angles)
+
+    if degree:
+        angles = np.deg2rad(angles)
+
+    table = np.zeros((Nt, angles.size))
+
+    h1 = np.abs(np.sin(angles) * h)
+    h2 = np.abs(np.cos(angles) * h)
+
+    a = np.max(h1 * (n1 + 1) / 2 + h2 * (n1 + 1) / 2 + s * (n2 + 1) / 2)
+
+    table = fft_trikernel(Nt, a, n1, n1, n2, h1, h2, s, pad_fact)
+
+    return table, a
