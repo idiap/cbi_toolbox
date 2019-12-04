2e69c74f461b180d7cb007292d3fdb82a489d277
François Marelli <mfrancois@fm-destroyer>
2019-12-04 12:33:37 +0100

    Renaming splradon to radon.

--- examples/measure_cuda.py
+++ examples/measure_cuda.py
@@ -15,7 +15,7 @@ else:
     print('Running on CPU')
 
 start = time.time()
-sinogram = cbi.splineradon.splradon(image, use_cuda=use_cuda)
+sinogram = cbi.splineradon.radon(image, use_cuda=use_cuda)
 stop = time.time()
 
 print('Radon : {}'.format(stop-start))
@@ -23,7 +23,7 @@ print('Radon : {}'.format(stop-start))
 del image
 
 start = time.time()
-reconstruct = cbi.splineradon.spliradon(sinogram, use_cuda=use_cuda)
+reconstruct = cbi.splineradon.iradon(sinogram, use_cuda=use_cuda)
 stop = time.time()
 
 print('IRadon : {}'.format(stop-start))
--- tests/test_splradon.py
+++ tests/test_splradon.py
@@ -1,7 +1,7 @@
 import numpy as np
 import unittest
 
-import cbi_toolbox
+import cbi_toolbox.splineradon as splradon
 
 
 class TestSplradon(unittest.TestCase):
@@ -11,13 +11,13 @@ class TestSplradon(unittest.TestCase):
         image_3d = np.random.rand(*image_dim)
         image_2d = np.copy(image_3d[..., 0])
 
-        spline_3d = cbi_toolbox.splradon(image_3d)
-        spline_2d = cbi_toolbox.splradon(image_2d)
+        spline_3d = splradon.radon(image_3d)
+        spline_2d = splradon.radon(image_2d)
 
         self.assertTrue(np.allclose(spline_2d, spline_3d[..., 0], rtol=1e-12, atol=1e-12))
 
-        out_3d = cbi_toolbox.spliradon(image_3d)
-        out_2d = cbi_toolbox.spliradon(image_2d)
+        out_3d = splradon.iradon(image_3d)
+        out_2d = splradon.iradon(image_2d)
 
         self.assertTrue(np.allclose(out_2d, out_3d[..., 0], rtol=1e-12, atol=1e-12))
 
@@ -28,15 +28,16 @@ class TestSplradon(unittest.TestCase):
         image_3d = np.transpose(image_3d, (1, 0, 2))
         image_2d = np.copy(image_3d[..., 0])
 
-        spline_3d = cbi_toolbox.splradon(image_3d)
-        spline_2d = cbi_toolbox.splradon(image_2d)
+        spline_3d = splradon.radon(image_3d)
+        spline_2d = splradon.radon(image_2d)
 
         self.assertTrue(np.allclose(spline_2d, spline_3d[..., 0], rtol=1e-9, atol=1e-12))
 
-        out_3d = cbi_toolbox.spliradon(spline_3d)
-        out_2d = cbi_toolbox.spliradon(spline_2d)
+        out_3d = splradon.iradon(spline_3d)
+        out_2d = splradon.iradon(spline_2d)
 
         self.assertTrue(np.allclose(out_2d, out_3d[..., 0], rtol=1e-9, atol=1e-12))
+
 
 if __name__ == "__main__":
     unittest.main()
