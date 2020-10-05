81ffdd7375f258562c72e235385bab048992833b
M. François <fmarelli@idiap.ch>
2020-10-05 17:55:20 +0200

    Fix deconv unknown filter

--- cbi_toolbox/reconstruct/deconv.py
+++ cbi_toolbox/reconstruct/deconv.py
@@ -2,7 +2,7 @@ from scipy import fft
 import numpy as np
 
 
-def inverse_psf_rfft(psf, shape=None, l=20, mode='laplacian'):
+def inverse_psf_rfft(psf, shape=None, l=20):
     """
     Computes the real FFT of a regularized inversed 2D PSF (or projected 3D)
     This follows the convention of fft.rfft: only half the spectrum is computed
@@ -16,8 +16,6 @@ def inverse_psf_rfft(psf, shape=None, l=20, mode='laplacian'):
         (if None, will be the same as the PSF), by default None
     l : int, optional
         regularization lambda, by default 20
-    mode : str, optional
-        what filter to use for regularization, by default 'laplacian'
 
     Returns
     -------
@@ -40,26 +38,13 @@ def inverse_psf_rfft(psf, shape=None, l=20, mode='laplacian'):
     psf = fft.ifftshift(psf)
     psf_fft = fft.rfft2(psf, s=shape, overwrite_x=True)
 
-    if mode == 'laplacian':
-        filt = [[0, -0.25, 0], [-0.25, 1, -0.25], [0, -0.25, 0]]
-        reg = np.abs(fft.rfft2(filt, s=shape)) ** 2
-
-    else:
-        filt1 = [1, -1, 0]
-        filt2 = [1, 0, -1]
-
-        reg_x = np.abs(
-            fft.fft(filt1, n=shape[0])) ** 2 + np.abs(fft.fft(filt2, n=shape[0])) ** 2
-
-        reg_y = np.abs(
-            fft.rfft(filt1, n=shape[1])) ** 2 + np.abs(fft.rfft(filt2, n=shape[1])) ** 2
-
-        reg = reg_x[:, None] + reg_y[None, :]
+    filt = [[0, -0.25, 0], [-0.25, 1, -0.25], [0, -0.25, 0]]
+    reg = np.abs(fft.rfft2(filt, s=shape)) ** 2
 
     return psf_fft.conjugate() / (np.abs(psf_fft) ** 2 + l * reg)
 
 
-def deconvolve_sinogram(sinogram, psf, **kwargs):
+def deconvolve_sinogram(sinogram, psf, l=20):
     """
     Deconvolve a sinogram with given PSF
 
@@ -69,13 +54,15 @@ def deconvolve_sinogram(sinogram, psf, **kwargs):
         the blurred sinogram
     psf : array [ZXY] or [XY]
         the PSF used to deconvolve
+    l : float, optional
+        strength of the regularization
 
     Returns
     -------
     array [TPY]
         the deconvolved sinogram
     """
-    inverse = inverse_psf_rfft(psf, shape=sinogram.shape[1:], **kwargs)
+    inverse = inverse_psf_rfft(psf, shape=sinogram.shape[1:], l=l)
     s_fft = fft.rfft2(sinogram)
 
     return fft.irfft2(s_fft * inverse)
@@ -93,7 +80,6 @@ if __name__ == '__main__':
         npix_axial=TEST_SIZE+1, npix_lateral=TEST_SIZE+1)
 
     i_psf = inverse_psf_rfft(s_psf)
-    i_psf_o = inverse_psf_rfft(s_psf, filter='other')
 
     psfft = fft.rfft2(s_psf.sum(0))
 
@@ -105,17 +91,12 @@ if __name__ == '__main__':
     s_fpsopt = imaging.fps_opt(sample, s_psf, theta=s_theta)
 
     s_deconv = deconvolve_sinogram(s_fpsopt, s_psf)
-    s_deconv_o = deconvolve_sinogram(s_fpsopt, s_psf, filter='other')
 
     with napari.gui_qt():
         viewer = napari.view_image(s_radon)
         viewer.add_image(s_fpsopt)
         viewer.add_image(s_deconv)
-        viewer.add_image(s_deconv_o)
 
         viewer = napari.view_image(fft.fftshift(
-            np.abs(i_psf), 0), name='Laplacian')
-        viewer.add_image(fft.fftshift(np.abs(i_psf_o), 0), name='Other')
-
-        viewer = napari.view_image(fft.irfft2(psfft * i_psf), name='Laplacian')
-        viewer.add_image(fft.irfft2(psfft * i_psf_o), name='Other')
+            np.abs(i_psf), 0))
+        viewer.add_image(fft.irfft2(psfft * i_psf))
