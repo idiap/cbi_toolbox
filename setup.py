f075f122c07736b2a1d84a7bdd94715b510e5376
M. Francois <mfrancois@fm-destroyer>
2020-11-19 15:00:31 +0100

    Fix pip install not copying packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import platform
 import subprocess
 import sys
 
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 
 __version__ = '0.1'
@@ -39,19 +39,25 @@ class CMakeBuild(build_ext):
                                ", ".join(e.name for e in self.extensions))
 
         for ext in self.extensions:
-            self.build_extension(ext)
+            if isinstance(ext, CMakeExtension):
+                self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        extdir = os.path.abspath(os.path.dirname(
+            self.get_ext_fullpath(ext.name)))
+
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
 
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      ]
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg, '--clean-first']
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += [
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             if sys.maxsize > 2 ** 32:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
@@ -68,8 +74,10 @@ class CMakeBuild(build_ext):
 
         os.makedirs(self.build_temp)
 
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] +
+                              cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] +
+                              build_args, cwd=self.build_temp)
 
 
 setup(
@@ -79,7 +87,10 @@ setup(
     author_email='francois.marelli@idiap.ch',
     description='A python toolbox for computational bioimaging',
     long_description='',
-    ext_modules=[CMakeExtension('cbi_toolbox/splineradon')],
+    ext_modules=[
+        CMakeExtension('cbi_toolbox.cmake_ext'),
+    ],
+    packages=find_packages(exclude=('tests', )),
     install_requires=requires,
     extras_require=extras_require,
     cmdclass={'build_ext': CMakeBuild},
