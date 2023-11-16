e39c200b2d7bb6366323667f62564129eff2ab6a
M. François <fmarelli@idiap.ch>
2023-11-16 14:38:57 +0100

    Add header to OMMTC_2024

--- experiments/OMMTC_2024/src/tools.py
+++ experiments/OMMTC_2024/src/tools.py
@@ -1,3 +1,21 @@
+# Copyright (c) 2023 Idiap Research Institute, http://www.idiap.ch/
+# Written by François Marelli <francois.marelli@idiap.ch>
+#
+# This file is part of CBI Toolbox.
+#
+# CBI Toolbox is free software: you can redistribute it and/or modify
+# it under the terms of the 3-Clause BSD License.
+#
+# CBI Toolbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# 3-Clause BSD License for more details.
+#
+# You should have received a copy of the 3-Clause BSD License along
+# with CBI Toolbox. If not, see https://opensource.org/licenses/BSD-3-Clause.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 import numpy as np
 import scipy.interpolate
 
